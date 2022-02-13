5d7685cfdb59d988ac418eec035a1ebc873ee3c5
RadjaIqbalSH <radjaiqbal.harahap@gmail.com>
2022-02-13 18:47:26 +0700

    delete layout dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   mode: 'jit',
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}',  './layouts/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
