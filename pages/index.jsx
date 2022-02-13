7ddb5b57fab45e54601fed557742d0d33557d12e
RadjaIqbalSH <radjaiqbal.harahap@gmail.com>
2022-02-13 18:30:42 +0700

    change tailwind config

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
 
 import Page from '../layouts/Page'
 import Hero from '../components/hero/Hero'
